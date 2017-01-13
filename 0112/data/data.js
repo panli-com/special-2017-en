bf93335df78a607860ee32c96e4c25bf0b300dab
zanjs <root@zanjs.com>
2017-01-13 09:50:25 +0800

    :cat: init

---
+++ 0112/data/data.js
@@ -0,0 +1,410 @@
+var productData = [
+  {
+    "商品类型": "Food",
+    "TB链接": "http://www.panlishop.com/Product/List.html?keyword=%E8%BF%9B%E5%8F%A3%E9%A3%9F%E5%93%81",
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.174.jVIlpP&id=37086431551&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/37086431551.html",
+    "商品价格(￥)": 22.8,
+    "USA": 3.49,
+    "简短商品名": "Turpan Raisins"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://chaoshi.detail.tmall.com/item.htm?spm=a230r.1.14.27.8MMJ2K&id=26032612240&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/26032612240.html",
+    "商品价格(￥)": 9.9,
+    "USA": 1.52,
+    "简短商品名": "Vietnam Dried Vegetables "
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.152.CnCTXI&id=2941640552&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/2941640552.html",
+    "商品价格(￥)": 4.7,
+    "USA": 0.72,
+    "简短商品名": "Thailand Coconut Slice"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.160.jVIlpP&id=43264181336&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/43264181336.html",
+    "商品价格(￥)": 25.8,
+    "USA": 3.95,
+    "简短商品名": "Fujian Mochi"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://chaoshi.detail.tmall.com/item.htm?spm=a230r.1.14.13.8MMJ2K&id=521082148996&cm_id=140105335569ed55e27b&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/521082148996.html",
+    "商品价格(￥)": 29.9,
+    "USA": 4.58,
+    "简短商品名": "Malaysia Durian Cake"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.111.jGijd1&id=524671867704&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/524671867704.html",
+    "商品价格(￥)": 185,
+    "USA": 28.33,
+    "简短商品名": "Hongkong Cookies"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://chaoshi.detail.tmall.com/item.htm?spm=a230r.1.14.6.SDPoCd&id=521232864010&cm_id=140105335569ed55e27b&abbucket=19&skuId=3105736638821",
+    "站内链接": "http://www.panlishop.com/product/detail/521232864010.html",
+    "商品价格(￥)": 79,
+    "USA": 12.1,
+    "简短商品名": "Japanese Cookies "
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://chaoshi.detail.tmall.com/item.htm?spm=a230r.1.14.21.v0Zm2k&id=43216782525&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/43216782525.html",
+    "商品价格(￥)": 10.5,
+    "USA": 1.61,
+    "简短商品名": "Korean Seaweed"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.54.ncrEHO&id=520084941587&ns=1&abbucket=19&skuId=3100397389331",
+    "站内链接": "http://www.panlishop.com/product/detail/520084941587.html",
+    "商品价格(￥)": 45,
+    "USA": 6.89,
+    "简短商品名": "Shanghai Spiced Beans"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.6.hVao4J&id=41930473335&cm_id=140105335569ed55e27b&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/41930473335.html",
+    "商品价格(￥)": 14.9,
+    "USA": 2.28,
+    "简短商品名": "Philippines Dried Mango"
+  },
+  {
+    "商品类型": "Costumes",
+    "TB链接": "http://www.panlishop.com/Product/List.html?keyword=%E6%B0%91%E6%97%8F%E6%9C%8D%E9%A5%B0",
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.58.P5bRCF&id=42448580025&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/42448580025.html",
+    "商品价格(￥)": 398,
+    "USA": 60.95,
+    "简短商品名": "Chinese Tangzhuang "
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.50.3Uz31j&id=535557297340&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/535557297340.html",
+    "商品价格(￥)": 93,
+    "USA": 14.24,
+    "简短商品名": "Muslim Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.16.sJAb7y&id=542157116407&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/542157116407.html",
+    "商品价格(￥)": 68,
+    "USA": 10.41,
+    "简短商品名": "Korean Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.243.3Uz31j&id=43038491344&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/43038491344.html",
+    "商品价格(￥)": 125,
+    "USA": 19.14,
+    "简短商品名": "Mongolian Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.45.IU561q&id=539629361065&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/539629361065.html",
+    "商品价格(￥)": 28,
+    "USA": 4.29,
+    "简短商品名": "Song Dynasty Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.110.RG92ge&id=528261800526&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/528261800526.html",
+    "商品价格(￥)": 85,
+    "USA": 13.02,
+    "简短商品名": "Japanese kimono"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.118.E06gox&id=527452369114&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/527452369114.html",
+    "商品价格(￥)": 199,
+    "USA": 30.47,
+    "简短商品名": "Thailand Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.147.KHwJ1p&id=536054059133&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/536054059133.html",
+    "商品价格(￥)": 288.12,
+    "USA": 44.12,
+    "简短商品名": "Cheongsam"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.158.k3QqN7&id=540386797691&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/540386797691.html",
+    "商品价格(￥)": 96,
+    "USA": 14.7,
+    "简短商品名": "Court Costume"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.66.IU561q&id=539471272653&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/539471272653.html",
+    "商品价格(￥)": 42.8,
+    "USA": 6.55,
+    "简短商品名": "Hanfu"
+  },
+  {
+    "商品类型": "Artware",
+    "TB链接": "http://www.panlishop.com/Product/List.html?keyword=%E5%B7%A5%E8%89%BA%E5%93%81",
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.340.O7qwoH&id=534723577496&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/534723577496.html",
+    "商品价格(￥)": 56,
+    "USA": 8.58,
+    "简短商品名": "Decoration "
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.13.LVeGF8&id=520961703882&cm_id=140105335569ed55e27b&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/520961703882.html",
+    "商品价格(￥)": 46,
+    "USA": 7.04,
+    "简短商品名": "Earrings"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.154.LVeGF8&id=8325543396&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/8325543396.html",
+    "商品价格(￥)": 5.6,
+    "USA": 0.86,
+    "简短商品名": "Porcelain"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://detail.tmall.com/item.htm?spm=a230r.1.14.125.LVeGF8&id=536720787695&ns=1&abbucket=19",
+    "站内链接": "http://www.panlishop.com/product/detail/536720787695.html",
+    "商品价格(￥)": 1,
+    "USA": 0.15,
+    "简短商品名": "Coasters"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.240.vv9gBZ&id=538777700359&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/538777700359.html",
+    "商品价格(￥)": 508,
+    "USA": 77.79,
+    "简短商品名": "Purse"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.158.zVokdj&id=13673302625&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/13673302625.html",
+    "商品价格(￥)": 10,
+    "USA": 1.53,
+    "简短商品名": "Key Chain"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.130.pYE14v&id=40775438641&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/40775438641.html",
+    "商品价格(￥)": 39.8,
+    "USA": 6.09,
+    "简短商品名": "Doll"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.60.LMDTPo&id=6819097423&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/6819097423.html",
+    "商品价格(￥)": 28.8,
+    "USA": 4.41,
+    "简短商品名": "Decorative Drum"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.53.G9uOSo&id=17445115863&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/17445115863.html",
+    "商品价格(￥)": 138,
+    "USA": 21.13,
+    "简短商品名": "Decoration"
+  },
+  {
+    "商品类型": null,
+    "TB链接": "https://item.taobao.com/item.htm?spm=a230r.1.14.99.UieMn3&id=521957137577&ns=1&abbucket=19#detail",
+    "站内链接": "http://www.panlishop.com/product/detail/521957137577.html",
+    "商品价格(￥)": 9.9,
+    "USA": 1.52,
+    "简短商品名": "Tableware "
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  },
+  {
+    "商品类型": null,
+    "TB链接": null,
+    "站内链接": null,
+    "商品价格(￥)": null,
+    "USA": null,
+    "简短商品名": null
+  }
+]
